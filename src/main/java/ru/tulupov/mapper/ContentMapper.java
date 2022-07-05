5bb3d0e7b5d87f5929651e9d7a7e43e0a2338c31
mikhail <kd.maikl-201@yandex.ru>
2022-07-05 11:05:25 +0700

    Adding annotations

--- src/main/java/ru/tulupov/mapper/ContentMapper.java
+++ src/main/java/ru/tulupov/mapper/ContentMapper.java
@@ -1,6 +1,8 @@
 package ru.tulupov.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 import org.mapstruct.factory.Mappers;
 import ru.tulupov.model.Content;
 import ru.tulupov.model.web.WebContent;
@@ -14,8 +16,16 @@ import java.util.List;
 public interface ContentMapper {
     ContentMapper INSTANCE = Mappers.getMapper(ContentMapper.class);
 
+    @Mappings({
+            @Mapping(target = "id", source = "webContent.id"),
+            @Mapping(target = "pages", source = "webContent.pages")
+    })
     Content webContentToContent(WebContent webContent);
 
+    @Mappings({
+            @Mapping(target = "id", source = "content.id"),
+            @Mapping(target = "pages", source = "content.pages")
+    })
     WebContent contentToWebContent(Content content);
 
     List<WebContent> contentsToWebContents(List<Content> contents);
