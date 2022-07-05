f9fb73f6714d8a3bdfd5c8ed7ddb14f5c0944b94
mikhail <kd.maikl-201@yandex.ru>
2022-07-05 11:05:47 +0700

    Adding static implementation

--- src/main/java/ru/tulupov/mapper/ViewedMapper.java
+++ src/main/java/ru/tulupov/mapper/ViewedMapper.java
@@ -1,19 +1,42 @@
 package ru.tulupov.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Named;
 import org.mapstruct.factory.Mappers;
+import ru.tulupov.model.Content;
+import ru.tulupov.model.User;
 import ru.tulupov.model.Viewed;
 import ru.tulupov.model.web.WebViewed;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @Mapper
 public interface ViewedMapper {
-    ViewedMapper INSTANCE = Mappers.getMapper(ViewedMapper.class);
 
-    Viewed webViewedToViewed(WebViewed webViewed);
+    @Named("webViewedToViewed")
+    static Viewed webViewedToViewed(WebViewed webViewed) {
 
-    WebViewed viewedToWebViewed(Viewed viewed);
+        return Viewed.builder()
+                .user(User.builder().id(UUID.fromString(webViewed.getUser())).build())
+                .content(Content.builder().id(UUID.fromString(webViewed.getContent())).build()).build();
+    }
 
-    List<WebViewed> viewedListToWebViewedList(List<Viewed> viewed);
+    @Named("viewedToWebViewed")
+    static WebViewed viewedToWebViewed(Viewed viewed) {
+        return WebViewed.builder()
+                .user(viewed.getUser().getId().toString())
+                .content(viewed.getContent().getId().toString()).build();
+    }
+
+    @Named("viewedListToWebViewedList")
+    static List<WebViewed> viewedListToWebViewedList(List<Viewed> viewed) {
+        List<WebViewed> webViewed = new ArrayList<>();
+        for (Viewed view: viewed) {
+            WebViewed v = ViewedMapper.viewedToWebViewed(view);
+            webViewed.add(v);
+        }
+        return webViewed;
+    }
 }
